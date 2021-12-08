de1469b585a23efd45799c3d1c35f24f1a64f7b3
calixto <raymondcalixto@gmail.com>
2021-12-07 20:15:53 -0500

    Primer commint3

---
+++ src/GifExpertApp.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     setCategories(['HunterXHunter', ...categories]);        
+    // }
+
+    
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+            {/* <button onClick={handleAdd}>Agregar</button> */}
+
+            <ol>
+                {
+                    categories.map( category =>(
+
+                        <GifGrid 
+                            key={category}
+                            category={category}                            
+                        />        
+
+                    ))
+                }
+                
+            </ol>
+        </>
+    )
+}
